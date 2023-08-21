6af589a0ec8e99751c8efaac4d9151badc116a72
bahri <saiful.bahri.tl@gmail.com>
2023-08-21 09:34:13 +0700

    model factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Task;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(2)->unverified()->create();
+        Task::factory(20)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
