3737ce92a9cb911269e7e727c251a4cf152894ee
bahri <saiful.bahri.tl@gmail.com>
2023-08-21 10:12:26 +0700

    validate & storing data

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,7 +5,9 @@ working create view
 @section('title', 'Add Task')
 
 @section('content')
+    {{ $errors }}
     <form method="POST" action="{{ route('tasks.store') }}">
+
         @csrf
         <div>
             <label for="title">
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use App\Models\Task as ModelsTask;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+
 use Illuminate\Support\Facades\Route;
-use PhpParser\Node\Expr\FuncCall;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,64 +17,13 @@ use PhpParser\Node\Expr\FuncCall;
 |
 */
 
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {
-    }
-}
 
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
 
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
-Route::get('/tasks', function () use ($tasks) {
+Route::get('/tasks', function () {
     return view(
         'index',
         [
@@ -86,7 +35,7 @@ Route::get('/tasks', function () use ($tasks) {
 Route::view('/tasks/create', 'create')
     ->name('tasks.create');
 
-Route::get('/tasks/{id}', function ($id) use ($tasks) {
+Route::get('/tasks/{id}', function ($id) {
     // $task = collect($tasks)->firstWhere('id', $id);
     // if (!$task) {
     //     abort(Response::HTTP_NOT_FOUND);
@@ -98,7 +47,18 @@ Route::get('/tasks/{id}', function ($id) use ($tasks) {
 })->name('tasks.show');
 
 Route::post('/tasks', function (Request $request) {
-    dd($request->all());
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required',
+    ]);
+    $task = new ModelsTask;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+    return redirect()->route('tasks.show', ['id' => $task->id]);
 })->name('tasks.store');
 
 // Route::get('/sss', function () {
