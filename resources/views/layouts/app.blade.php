e9a75eb8748551c73df5435814f58fecc74256ac
bahri <saiful.bahri.tl@gmail.com>
2023-08-21 13:50:52 +0700

    apline.js for flash

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,10 +37,22 @@
         @yield('title')
     </h1>
 
-    <div>
+    <div x-data="{ flash: true }">
         @if (session()->has('success'))
-            <div>{{ session('success') }}</div>
+            <div x-show="flash"
+                class="relative mb-10 rounded border border-green-400 bg-green100 px-4 py-3 text-lg text-green-700"
+                role='alert'>
+                <strong>Success</strong>
+                <div>{{ session('success') }}</div>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        @click="flash = false" stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </span>
+            </div>
         @endif
+
         @yield('content')
     </div>
 </body>
